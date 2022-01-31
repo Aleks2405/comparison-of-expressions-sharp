ffa7e9341da24a151e7beb46a48ae554c28b20a7
Aleksandr <shura.gorodnichev@mail.ru>
2022-02-01 01:10:49 +0300

    Добавьте файлы проекта.

---
+++ сравнение чисел/Class1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace You
+{
+    class Numbers
+    {
+       private string number;
+        public char[] sumbol;
+        public bool znak = true;
+        //public string Number
+        //{
+        //    get { return number != null ? number : "0"; }
+        //    set { number = value; }     // подсматрел из учебника но почему тут постоянно всплывает value пока не понятно 
+        //}
+
+        public Numbers( string number) {
+            this.number = number;
+
+        }
+        public bool Znak (char []sumbol,char a)
+        {
+            return znak = (int)sumbol[0] > (int) sumbol[2] ? true : false;
+        }
+        public bool Znak_1(char[] sumbol, char a)
+        {
+            return znak = (int)sumbol[0] < (int)sumbol[2] ? true : false;
+        }
+        public bool Znak_2(char[] sumbol, char a)
+        {
+            return znak = (int)sumbol[0] >= (int)sumbol[2] ? true : false;
+        }
+        public bool Znak_3(char[] sumbol, char a)
+        {
+            return znak = (int)sumbol[0] <= (int)sumbol[2] ? true : false;
+        }
+
+
+        public bool Print ()
+        {
+            return znak;
+        }
+    }
+}
---
+++ сравнение чисел/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using You;
+/*Пользователь вводит в строку с клавиатуры логическое 
+выражение. Например, 3>2 или 7<3. Программа должна 
+посчитать результат введенного выражения и дать результат true или false. В строке могут быть только целые числа 
+и операторы: <, >, <=, >=, ==, !=. Для обработки ошибок 
+ввода используйте механизм исключений*/
+namespace You
+{
+    class Program
+    {
+        static string myFunc(int x, int y) => x > y ? ">" : x < y ? "<" : "=";
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите Ваше выражение");
+            string stroka;
+            stroka = Console.ReadLine();
+            Numbers One = new Numbers(stroka);
+            //bool znak = true;
+            char asd = '0';
+            
+
+            char[] sumbol = stroka.ToCharArray();
+            for (int i = 0; i < stroka.Length; i++)
+            {
+                if (sumbol[i] == '>' || sumbol[i] == '<'|| sumbol[i] == '=')
+                {
+                    asd = sumbol[i];
+                }
+            }
+            One.Znak(sumbol, asd);
+            One.Znak_1(sumbol, asd);
+            One.Znak_2(sumbol, asd);
+           // One.Znak_3(sumbol, asd);
+
+            //       znak = (int)sumbol[0] > (int)sumbol[2] ? true : false;
+
+            //    } else if (sumbol[i] == '<')
+            //    {
+            //        Console.WriteLine("prov1");
+            //        znak = (int)sumbol[0] < (int)sumbol[2] ? true : false;
+            //    }
+
+            //}
+
+
+            Console.WriteLine(One.Print());
+          
+        }
+    }
+}
